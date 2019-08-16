b6ca1fcab4c9bc5ebeaf65b0625aace4aa069b36
Sarah Tatro <statrox4@gmail.com>
2019-08-16 15:40:51 -0600

    Items most revenue BI;

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,14 @@ class Item < ApplicationRecord
                         :description,
                         :unit_price
 
-  def most_revenue(quantity)
+  def self.most_revenue(quantity)
+    Item.joins(:invoices)
+    .joins("Join invoice_items ON invoices.id = invoice_items.invoice_id")
+    .joins("Join transactions ON invoices.id = transactions.invoice_id")
+    .select("items.*, SUM(invoice_items.unit_price * invoice_items.quantity) AS revenue")
+    .where("transactions.result = ?", "success")
+    .group(:id).order("revenue desc")
+    .limit(quantity)
   end
 
 end
